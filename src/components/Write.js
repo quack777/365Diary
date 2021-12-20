a0cd4c2e8f06edff77b3b11137516b776cbb8343
aksel26 <kevinxkim2023@gmail.com>
2021-12-21 05:05:26 +0900

    [내 일기장] 새 글쓰기 컴포넌트로 분류

--- src/components/Write.js
+++ src/components/Write.js
@@ -8,11 +8,14 @@ import axios, { Axios } from "axios";
 import "../styles/Write.css";
 
 function Write() {
+  const location = useLocation();
+  const history = useHistory();
+
   const NewDate = new Date();
   const month = NewDate.getMonth() + 1;
   const date = NewDate.getDate();
   const year = NewDate.getFullYear();
-  
+
   const [question, setQuestion] = useState("나의 삶의 목적은 무엇인가요?");
   const [answer, setAnswer] = useState("");
   const [count, setCount] = useState(0);
@@ -24,79 +27,47 @@ function Write() {
   const [cMonth, setCMonth] = useState();
   const [cDate, setCDate] = useState();
   const [cYear, setCYear] = useState();
-  
+  const [content, setContent] = useState("");
+
   const loca = useLocation();
-  console.log(loca)
-  const match = useRouteMatch();
-  const paramsQN = match.params.question_num;
-  console.log(paramsQN)
+
+  const now = new Date();
+  const start = new Date(now.getFullYear(), 0, 0);
+  const diff = now - start;
+  const oneDay = 1000 * 60 * 60 * 24;
+  const day = Math.floor(diff / oneDay) + 1;
 
   function writeSubmit(e) {
-    if(paramsQN === undefined) {
-      e.preventDefault();
-      console.log(answer);
-      const inputValue = e.target[0].value;
-      e.target[0].value = "";
-      console.log("submit");
-      setCount(0);
-      setAnswer("");
-  
-      const now = new Date();
-      const year = String(now.getFullYear());
-      const month = String(now.getMonth() + 1).padStart(2, "0");
-      const date = String(now.getDate()).padStart(2, "0");
-      const answerDate = `${month}${date}`;
-  
-      console.log(year);
-      console.log(answerDate);
-      console.log(answer);
-      console.log(publica);
-      console.log(questionN);
-      console.log(typeof member);
-      axios
-        .post("http://61.72.99.219:9130/answers/new", {
-          answer_year: year,
-          answer_date: answerDate,
-          answer: answer,
-          public_answer: publica,
-          question_num: questionN,
-          member_num: member,
-        })
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    } else {
-      e.preventDefault();
-      axios({
-          url: `/answers/pages/${loca.state.data.answer_num}/1`,
-          // /answers/pages/{answer_num}/{member_num}
-          method: "patch",
-          baseURL: "http://61.72.99.219:9130/",
-          data: {
-            answer : answer,
-            public_answer : loca.state.data.public_answer,
-            answer_num: loca.state.data.answer_num,
-            member_num: loca.state.data.member_num
-          }
-        })
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
+    e.preventDefault();
+    const now = new Date();
+    const year = String(now.getFullYear());
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const date = String(now.getDate()).padStart(2, "0");
+    const answerDate = `${month}${date}`;
+
+    axios
+      .post("http://61.72.99.219:9130/answers/new", {
+        answer_year: year,
+        answer_date: answerDate,
+        answer: content,
+        public_answer: publica,
+        question_num: questionN,
+        member_num: 1, //임시
+      })
+      .then(function (response) {
+        if (response.status === 200) alert("글 등록 완료");
+        history.push("/list");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   function inputCount(e) {
     const inputValue = e.target.value;
+    setContent(inputValue);
     let count = inputValue.length;
     // 한글이랑 영어 카운터 다름 해결필요
-    setAnswer(inputValue);
-    console.log(answer);
     setCount(count);
   }
 
@@ -110,54 +81,22 @@ function Write() {
     setPublica("Y");
   }
 
-  function sendData() {
-    console.log(answer);
-  }
-
   useEffect(() => {
-    if(paramsQN === undefined) {
-      const now = new Date();
-      const start = new Date(now.getFullYear(), 0, 0);
-      const diff = now - start;
-      const oneDay = 1000 * 60 * 60 * 24;
-      const day = Math.floor(diff / oneDay) + 1;
-  
-      const member_num = localStorage.getItem("member_num");
-      setMember(Number(member_num));
-  
-      axios({
-        url: `/question/${day}`,
-        method: "get",
-        baseURL: "http://61.72.99.219:9130",
-        //withCredentials: true,
-      })
-        .then(function (response) {
-          console.log(response.data);
-          setQuestion(response.data.question);
-          setQuestionN(response.data.question_num);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    } else {
-      axios({
-        url: `/question/calendars/${paramsQN}`,
-        method: "get",
-        baseURL: "http://61.72.99.219:9130",
-        //withCredentials: true,
-      })
-        .then(function (response) {
-          console.log(response.data);
-          setQuestion(response.data.question);
-          setQuestionN(response.data.question_num);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-    
-  }, []);
+    const member_num = localStorage.getItem("member_num");
+    setMember(Number(member_num));
 
+    axios
+      .get(`/question/calendars/${day}`, {
+        baseURL: "http://61.72.99.219:9130",
+      })
+      .then(function (response) {
+        setQuestion(response.data.question);
+        setQuestionN(response.data.question_num);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [day]);
 
   return (
     <div className="Write">
@@ -172,8 +111,9 @@ function Write() {
       <form onSubmit={writeSubmit} className="writeBox">
         <textarea
           onInput={inputCount}
+          value={content}
           maxLength="200"
-        >{loca?.state?.data.answer}</textarea>
+        ></textarea>
         <p>{count}/200</p>
         <div>
           <div className="private">
@@ -188,14 +128,9 @@ function Write() {
               <p id="first">작성취소</p>
             </Link>
             <button id="second" type="submit">
-                api임시테스트용
+              저장하기
             </button>
-            <Link to="/list">
-              <button id="second" type="submit">
-                저장하기
-              </button>
-            </Link>
-            </div>
+          </div>
         </div>
       </form>
     </div>
