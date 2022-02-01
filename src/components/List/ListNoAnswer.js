eeca97f46b6cb978307d75ed6bb95fd170d1fee8
aksel26 <kevinxkim2023@gmail.com>
2022-02-01 16:36:52 +0900

    [내 일기장] 버튼 컴포넌트 생성 및 CSS 개발

--- src/components/List/ListNoAnswer.js
+++ src/components/List/ListNoAnswer.js
@@ -1,7 +1,9 @@
 import React from "react";
 import noAnswer from "../../styles/images/listNoAnswer.png";
+import Button from "../Presentational/Button";
 export default function ListNoAnswer({ setQuestion }) {
   setQuestion("*해당 날짜에 작성된 일기가 없습니다.");
+  const btn_text = "오늘 날짜로 이동하기";
   return (
     <div className="noAnswer">
       <hr />
@@ -9,6 +11,8 @@ export default function ListNoAnswer({ setQuestion }) {
       <div className="noAnswerText">
         작성한 일기가 없어 해당 날짜의 질문을 확인할 수 없습니다.
       </div>
+
+      <Button btn_text={btn_text} />
     </div>
   );
 }
---
+++ src/components/Presentational/Button.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+
+export default function Button(props) {
+  const BtnLayout = {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "8px",
+  };
+  const BtnStyle = {
+    width: "218px",
+    height: "55.5px",
+    backgroundColor: "#7EB496",
+    borderRadius: "12px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "#FFFFFF",
+    fontSize: "17px",
+    lineHeight: "174.2%",
+  };
+
+  return (
+    <Link
+      to="/list"
+      onClick={() => window.location.reload()}
+      style={{ textDecoration: "none" }}
+    >
+      <div className="btn_section" style={BtnLayout}>
+        <div className="btn" style={BtnStyle}>
+          {props.btn_text}
+        </div>
+      </div>
+    </Link>
+  );
+}
