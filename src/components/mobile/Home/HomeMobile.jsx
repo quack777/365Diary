537f3ee5fc28b3ba506f32fa1d88105b6d652858
aksel26 <kevinxkim2023@gmail.com>
2022-02-07 01:47:37 +0900

    Add : [홈] 모바일_레이아웃 개발 완료

--- src/components/mobile/Home/HomeMobile.jsx
+++ src/components/mobile/Home/HomeMobile.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "../../../styles/mobile/Home.css"
 import bottomImg from "../../../styles/images_mobile/home_bottomImg_mobile.png"
-
+import createBtn from "../../../styles/images_mobile/home_createBtn_arrow.png"
 export default function Home() {
     return (
         <div className="home_container">
@@ -28,6 +28,7 @@ export default function Home() {
                         <div className="home_createBtn_text">
 
                             오늘 나의 생각 남기기
+                            <img src={createBtn} alt="" style={{paddingLeft:"7px"}} />
                         </div>
                     </div>
 
@@ -36,17 +37,20 @@ export default function Home() {
 
                 <div className="home_bottom_conatiner">
 
-                    <div className="home_bottom_text">
-                        <span>
+                    <div className="home_bottomText">
+                        <div className="home_bottomText_content">
                             365개의 질문들, <br/>
                             그리고 나오 나를 연결할 기록들.
-                        </span>
-                        <span>
-                        매일 달라지는 질문에 답해보세요. 3년 동안의 기록을 차곡차곡 모으고, 찬찬히 나를 만나봐요.
-                        </span>
+                        </div>
+
+                        <div className="home_bottomText_subContent">
+
+                        매일 달라지는 질문에 답해보세요.  <br/>
+                        3년 동안의 기록을 차곡차곡 모으고, 찬찬히 나를 만나봐요.
+                        </div>
                     </div>
                     <div className="home_bottom_img">
-                            <img src={bottomImg} alt="bottomImg" style={{width:"223px", height:"168px"}}/>
+                            <img src={bottomImg} alt="bottomIm g" style={{width:"223px", height:"168px"}}/>
                     </div>
                 </div>
             </div>
