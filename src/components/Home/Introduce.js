4de45b9878e35ebf46584156c3b3c09732ba782f
Mcdonalds Trump <whdmschd2@gmail.com>
2022-02-22 09:02:36 +0900

    Refactor: 소개 코드 정리

--- src/components/Home/Introduce.js
+++ src/components/Home/Introduce.js
@@ -1,69 +1,16 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useSwipeable } from "react-swipeable";
-import "../../styles/Introduce.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
 import Introduce_top from "../../styles/images/07.png";
 import ballon from "../../styles/images/Group 5582.png";
 import ballonMobile from "../../styles/images/ballon_mobile.png";
 import made_people from "../../styles/images/made_people.png";
 import made_people_mobile from "../../styles/images/made_people_mobile.png";
-import soga00 from "../../styles/images/soga00.png";
-import soga01 from "../../styles/images/soga01.png";
-import soga02 from "../../styles/images/soga02.png";
-import soga03 from "../../styles/images/soga03.png";
-import soga04 from "../../styles/images/soga04.png";
-import soga00Mobile from "../../styles/images/soga00_mobile.png";
-import soga01Mobile from "../../styles/images/soga01_mobile.png";
-import soga02Mobile from "../../styles/images/soga02_mobile.png";
-import soga03Mobile from "../../styles/images/soga03_mobile.png";
-import soga04Mobile from "../../styles/images/soga04_mobile.png";
 
-const slideData = [
-    {
-        img: soga00,
-        title: "회원가입/로그인",
-        pp: "카카오로 간편하게 회원가입과 로그인을 할 수 있어요.",
-    },
-    {
-        img: soga01,
-        title: "메인페이지",
-        pp: "공개 설정한 일기에 한해 랜덤으로 다른 사람들의 이야기를 볼 수 있어요.",
-    },
-    {
-        img: soga02,
-        title: "나의 일기장",
-        pp: "내 일기장에서 누적된 내 일기들을 날짜별로 모아 볼 수 있어요.",
-    },
-    {
-        img: soga03,
-        title: "휴지통",
-        pp: "삭제된 일기는 휴지통에 7일간 보관됩니다. 삭제되기 전, 다시 되돌릴 수도 있어요.",
-    },
-    {
-        img: soga04,
-        title: "잠깐!",
-        pp: "해당 날짜마다 질문이 달라져요. 답변은 그 날까지만 작성, 수정이 가능해요. 오늘의 질문을 놓치지 마세요!",
-    },
-];
-
-const slideDataMobile = [
-    {
-        img: soga00Mobile,
-    },
-    {
-        img: soga01Mobile,
-    },
-    {
-        img: soga02Mobile,
-    },
-    {
-        img: soga03Mobile,
-    },
-    {
-        img: soga04Mobile,
-    },
-];
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "../../styles/Introduce.css";
+import { slideData, slideDataMobile } from "./slideData";
 
 const Introduce = ({ isMobile }) => {
     const location = useLocation();
@@ -118,26 +65,18 @@ const Introduce = ({ isMobile }) => {
                     <button onClick={nextSlide}></button>
                 </div>
                 {isMobile
-                    ? slideDataMobile.map((slide, index) => {
-                          return (
-                              <div
-                                  className={index === current ? "slide active" : "slide"}
-                                  key={index}
-                                  {...mobileSwipe}
-                              >
-                                  {index === current && <img src={slide.img} className="image" />}
-                              </div>
-                          );
-                      })
-                    : slideData.map((slide, index) => {
-                          return (
-                              <div className={index === current ? "slide active" : "slide"} key={index}>
-                                  {index === current && <img src={slide.img} className="image" />}
-                                  {index === current && <p>{slide.title}</p>}
-                                  {index === current && <p>{slide.pp}</p>}
-                              </div>
-                          );
-                      })}
+                    ? slideDataMobile.map((slide, index) => (
+                          <div className={index === current ? "slide active" : "slide"} key={index} {...mobileSwipe}>
+                              {index === current && <img src={slide.img} className="image" />}
+                          </div>
+                      ))
+                    : slideData.map((slide, index) => (
+                          <div className={index === current ? "slide active" : "slide"} key={index} {...mobileSwipe}>
+                              {index === current && <img src={slide.img} className="image" />}
+                              {index === current && <p>{slide.title}</p>}
+                              {index === current && <p>{slide.pp}</p>}
+                          </div>
+                      ))}
                 <div className="introSlideNav">
                     {isMobile
                         ? slideDataMobile.map((slide, index) => {
---
+++ src/components/Home/slideData.js
@@ -0,0 +1,57 @@
+import soga00 from "../../styles/images/soga00.png";
+import soga01 from "../../styles/images/soga01.png";
+import soga02 from "../../styles/images/soga02.png";
+import soga03 from "../../styles/images/soga03.png";
+import soga04 from "../../styles/images/soga04.png";
+
+import soga00Mobile from "../../styles/images/soga00_mobile.png";
+import soga01Mobile from "../../styles/images/soga01_mobile.png";
+import soga02Mobile from "../../styles/images/soga02_mobile.png";
+import soga03Mobile from "../../styles/images/soga03_mobile.png";
+import soga04Mobile from "../../styles/images/soga04_mobile.png";
+
+export const slideData = [
+    {
+        img: soga00,
+        title: "회원가입/로그인",
+        pp: "카카오로 간편하게 회원가입과 로그인을 할 수 있어요.",
+    },
+    {
+        img: soga01,
+        title: "메인페이지",
+        pp: "공개 설정한 일기에 한해 랜덤으로 다른 사람들의 이야기를 볼 수 있어요.",
+    },
+    {
+        img: soga02,
+        title: "나의 일기장",
+        pp: "내 일기장에서 누적된 내 일기들을 날짜별로 모아 볼 수 있어요.",
+    },
+    {
+        img: soga03,
+        title: "휴지통",
+        pp: "삭제된 일기는 휴지통에 7일간 보관됩니다. 삭제되기 전, 다시 되돌릴 수도 있어요.",
+    },
+    {
+        img: soga04,
+        title: "잠깐!",
+        pp: "해당 날짜마다 질문이 달라져요. 답변은 그 날까지만 작성, 수정이 가능해요. 오늘의 질문을 놓치지 마세요!",
+    },
+];
+
+export const slideDataMobile = [
+    {
+        img: soga00Mobile,
+    },
+    {
+        img: soga01Mobile,
+    },
+    {
+        img: soga02Mobile,
+    },
+    {
+        img: soga03Mobile,
+    },
+    {
+        img: soga04Mobile,
+    },
+];
