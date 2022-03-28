c9d80af7d051bb8d50698c8fa3a7f34c92a7207d
Mcdonalds Trump <whdmschd2@gmail.com>
2022-03-28 23:28:47 +0900

    Refactor: 휴지통 모달 코드 수정

--- src/components/Trash/TrashOneDeleteModal.js
+++ src/components/Trash/TrashOneDeleteModal.js
@@ -2,26 +2,29 @@ import React from "react";
 import axios from "axios";
 import { DeleteModal } from "../util/alert_modal/DeleteModal";
 
-export default function TrashOneDeleteModal(props) {
+const TrashOneDeleteModal = ({ clickAN, member, posts, setPosts, setOpenDeleteModal, history }) => {
     const goTrash = () => {
         try {
-            axios.delete(`${process.env.REACT_APP_SERVER_IP}/trashes/${props.clickAN[0]}`, {
+            axios.delete(`${process.env.REACT_APP_SERVER_IP}/trashes/${clickAN[0]}`, {
                 data: {
-                    member_num: props.member,
-                    answer_delete: props.clickAN[1],
+                    member_num: member,
+                    answer_delete: clickAN[1],
                 },
             });
 
-            props.setPosts(props.posts.filter((data) => data.answer_num !== props.clickAN[0]));
-            props.setOpenDeleteModal(false);
+            setPosts(posts.filter((data) => data.answer_num !== clickAN[0]));
+            setOpenDeleteModal(false);
         } catch (error) {
             console.log("error: ", error);
-            props.history.push("/error");
+            history.push("/error");
         }
     };
 
-    function xDelete() {
-        props.setOpenDeleteModal(false);
-    }
+    const xDelete = () => {
+        setOpenDeleteModal(false);
+    };
+
     return <DeleteModal content={"일기를"} xDelete={xDelete} goTrash={goTrash} />;
-}
+};
+
+export default TrashOneDeleteModal;
